9a9d1ad79a07a6e2cbe1584bdc344402d28e8a7d
Valerio DAlessio <u105550@canda.com>
2026-06-22 08:55:56 +0200

    feat(core): Sockets - Add start_listening and accept_connection method

--- sockets/server.c
+++ sockets/server.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 
 #define PORT 5001
+#define QUEUE_SIZE 2
 
 /**
  * @brief Creates a new socket 
@@ -18,11 +19,10 @@ int create_socket(void)
 {
     const struct protoent *protoent = getprotoent();
     int server_sock = socket(AF_INET, SOCK_STREAM, protoent->p_proto); 
-    printf("%d\n", server_sock);
     
     if(server_sock == -1)
     {
-        fprintf(stderr, "Error opening the server socket using %s protocol\n", protoent->p_name);
+        fprintf(stderr, "Error: Opening the server socket using %s protocol\n", protoent->p_name);
         exit(EXIT_FAILURE);
     }
 
@@ -31,14 +31,15 @@ int create_socket(void)
 
 /**
  * @brief Creates a new socket address
+ * @param uint8_t port_num
  * @return struct sockaddr_in
  */
-struct sockaddr_in create_socket_address(void)
+struct sockaddr_in create_socket_address(uint16_t port_num)
 {
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = htonl(INADDR_ANY);
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port_num);
 
     return address;
 }
@@ -53,10 +54,9 @@ void bind_socket(int server_socket, struct sockaddr_in *address)
 {
     int bind_result = bind(server_socket, (struct sockaddr *)address, sizeof(*address));
 
-    printf("%d\n", bind_result);
     if(bind_result == -1)
     {
-        fprintf(stderr, "Error binding the address with the socket\n");
+        fprintf(stderr, "Error: Binding the address with the socket\n");
         close(server_socket);
         exit(EXIT_FAILURE);
     }
@@ -64,14 +64,57 @@ void bind_socket(int server_socket, struct sockaddr_in *address)
     printf("Socket successfully bound to port %d\n", PORT);
 }
 
+/**
+ * @brief Listens passively for incoming connections
+ * @param int server_socket: The socket file id 
+ * @param int backlog: The socket queue 
+ * @return void
+ */
+void start_listening(int server_socket, int backlog)
+{
+    // The listen() call applies only to sockets of type SOCK_STREAM
+    int listen_result = listen(server_socket, backlog);
+    
+    if(listen_result == -1)
+    {
+        fprintf(stderr, "Error: Server Socket could not change its state to listening\n");
+        // TODO
+        // ECONNREFUSED: if the underlying protocol supports retransmission, the request may be ignored so that retries may succeed
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server Socket started listening\n");
+}
+
+/**
+ * @brief Accepts an incoming connection by creating a new socket FD. 
+ * @param int server_socket
+ * @param struct sockaddr restrict address pointer
+ * @param socklen_t restrict address_len pointer
+ * @return void
+ */
+void accept_connection(int server_socket, struct sockaddr *restrict address)
+{
+    socklen_t address_len = sizeof(address);
+    int accept_result = accept(server_socket, address, &address_len);
+
+    if(accept_result == -1)
+    {
+        fprintf(stderr, "Error: Could not accept incoming connection\n");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(void)
 {
     int server_sock = create_socket();
-    struct sockaddr_in address = create_socket_address();
+    struct sockaddr_in address = create_socket_address(PORT);
     bind_socket(server_sock, &address);
-
+    start_listening(server_sock, QUEUE_SIZE);
+    accept_connection(server_sock, (struct sockaddr *)&address);
+    
     close(server_sock);
-
     return 0;
 }
