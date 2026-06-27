1231ec678877c73e1e6aa9ff8f6e082f2f9773ae
Valerio DAlessio <u105550@canda.com>
2026-06-27 08:48:39 +0200

    feat(core): Ring Buffer - Refactored the memory structure before start working on the ring buffer logic

--- pthread/ring_buffer.c
+++ pthread/ring_buffer.c
@@ -144,9 +144,33 @@ void init_tracking(F1Tracking **tracker, Telemetry **telemetry, TelemetryProcess
  * @param void arg pointer
  * @return void pointer
  */
-void *read_sensor_data(void *arg)
+void *write_sensor_data(void *arg)
 {
     F1Tracking *tracker = (F1Tracking *)arg;
+    validate_telemetry(tracker, TRACKER);
+    printf("Writing Sensor Data: Please wait...\n");
+
+    for(int i = 0; i < 10; i++)
+    {
+        sleep(1);
+
+        pthread_mutex_lock(&mutex);
+    
+        while(tracker->t->count == BUFFER_SIZE)
+        {
+            pthread_cond_wait(&not_full, &mutex);
+        }
+    
+        tracker->t->buffer[tracker->t->tail] = i;
+        tracker->t->tail = (tracker->t->tail + 1) % BUFFER_SIZE;
+        tracker->t->count += 1;
+        printf("W: [%d]\n", i);
+
+        pthread_mutex_unlock(&mutex);
+        // signal to the READER
+        pthread_cond_signal(&not_empty);
+    }
+
     return NULL;
 }
 
@@ -156,9 +180,32 @@ void *read_sensor_data(void *arg)
  * @param void arg pointer
  * @return void pointer
  */
-void *store_sensor_data(void *arg)
+void *read_sensor_data(void *arg)
 {
     F1Tracking *tracker = (F1Tracking *)arg;
+    validate_telemetry(tracker, TRACKER);
+    printf("Reading Sensor Data: Please wait...\n");
+    
+    for(int i = 0; i < 10; i++)
+    {
+        sleep(1);
+
+        pthread_mutex_lock(&mutex);
+        
+        while(tracker->t->count == 0)
+        {
+            pthread_cond_wait(&not_empty, &mutex);
+        }
+
+        uint8_t head_value = tracker->t->head;
+        tracker->t->head = (tracker->t->head + 1) % BUFFER_SIZE;
+        printf("R: [%d]\n", head_value);
+        
+        pthread_mutex_unlock(&mutex);
+        // signal to the WRITER
+        pthread_cond_signal(&not_full);
+    }
+    
     return NULL;
 }
 
@@ -181,34 +228,47 @@ void clean_up(F1Tracking *tracker)
     tracker = NULL;
 }
 
+/**
+ * @brief Spawns a new thread for reading the data 
+ * @parm F1Tracking tracker pointer
+ * @return void
+ */
 void start_reading(F1Tracking *tracker)
 {
-    printf("Start Reading Sensor Data\n");
     pthread_create(&tracker->p->read, NULL, read_sensor_data, (void *)tracker);
+}
+
+/**
+ * @brief Spawns a new thread for Writing the data 
+ * @parm F1Tracking tracker pointer
+ * @return void
+ */
+void start_writing(F1Tracking *tracker)
+{
+    pthread_create(&tracker->p->store, NULL, write_sensor_data, (void *)tracker);
 }
 
 int main(void)
 {
-    TelemetryType tracker = TRACKER;
-    TelemetryType pthread_reader = READER;
-    TelemetryType pthread_writer = WRITER;
-    
     TelemetryProcess *telemetry_process = NULL;
     Telemetry *telemetry = NULL;
     F1Tracking *f1_tracker = NULL;
 
-    init_telemetry(&telemetry, pthread_reader);
-    init_telemetry_process(&telemetry_process, pthread_writer);
+    init_telemetry(&telemetry, READER);
+    init_telemetry_process(&telemetry_process, WRITER);
     
-    init_tracking(&f1_tracker, &telemetry, &telemetry_process, tracker);
+    init_tracking(&f1_tracker, &telemetry, &telemetry_process, TRACKER);
     printf("F1Tracking allocated at address %p\n", f1_tracker);
     printf("Telemetry allocated at address %p\n", f1_tracker->t);
     printf("Telemetry Process allocated at address %p\n", f1_tracker->p);
 
+    // Spawn the pthreads for reading and writing
     start_reading(f1_tracker);
+    start_writing(f1_tracker);
 
     // Join and wait 
     pthread_join(f1_tracker->p->read, NULL);
+    pthread_join(f1_tracker->p->store, NULL);
 
     // Start the memory clean up 
     clean_up(f1_tracker);
