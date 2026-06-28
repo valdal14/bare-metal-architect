f482245f75e5dfedc77c3c1207f22e5cf65f156b
Valerio DAlessio <u105550@canda.com>
2026-06-28 18:43:04 +0200

    feat(core): POSIX Thread CONDITION Example - Add concurrent methods

--- pthread/water_pump.c
+++ pthread/water_pump.c
@@ -5,16 +5,16 @@
 #include <unistd.h>
 
 #define BIT(x) (1 << (x))
-#define DEFAULT_STATE 0x00 
+#define DEFAULT_STATE 0x02 
+#define READY_STATE 0x09
 #define READY_BIT 0
 #define EMPTY_BIT 1
 #define PROGRESS_BIT 3
-#define READ_LEVEL 50
 #define MAX_LEVEL 100
 
 // Concurrency Primitives
 pthread_cond_t is_ready = PTHREAD_COND_INITIALIZER;
-pthread_cond_t is_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t is_level = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef enum
@@ -35,7 +35,9 @@ typedef struct
      * 0 is_ready    bit 
      * 2 is_empty    bit
      * 4 in_progress bit 
+     * 1001
      */
+    uint8_t level;
     uint8_t state;
 } Pump;
 
@@ -92,17 +94,92 @@ void init(Pump **pump, Temp temp)
             break;
     }
    
+    new_pump->level = 0;
     new_pump->temp = 77;
     new_pump->state = DEFAULT_STATE;
     *pump = new_pump;
+}
+
+void *fill_water(void *arg)
+{
+    Pump *pump = (Pump *)arg;
+    if(pump == NULL) exit(EXIT_FAILURE);
+    pthread_mutex_lock(&mutex);
+    
+    while((pump->state & BIT(1)) != 0)
+    {
+        pump->level += 10;
+        // set the pump state ready 
+        if(pump->level >= MAX_LEVEL) 
+        {
+            pump->state &= ~(BIT(1));
+            pthread_cond_signal(&is_ready);
+        }
+    }
+    
+    pthread_mutex_unlock(&mutex);
+    pthread_cond_signal(&is_ready);
+    return (void *)pump;
+}
+
+void *get_water(void *arg)
+{
+    Pump *pump = (Pump *)arg;
+    if(pump == NULL) exit(EXIT_FAILURE);
+    printf("Waiting for the water level to reach min capacity\n");
+    pthread_mutex_lock(&mutex);
+    while((pump->state & READY_STATE) != 0) pthread_cond_wait(&is_ready, &mutex);
+    pthread_mutex_unlock(&mutex);
+     
+    // get water 
+    while((pump->state & BIT(1)) == 0)
+    {
+        printf("Getting 10 liters of water our of the tank [%d]\n", pump->level);
+        sleep(1);
+        pthread_mutex_lock(&mutex);
+        pump->level -= 10;
+        if(pump->level == 0)
+        {
+            // stop the pump form getting water 
+            pump->state |= BIT(1);
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+    return (void *)pump;
+}
+
+void *show_report(void *arg)
+{
+    Pump *pump = (Pump *)arg;
+    if(pump == NULL) exit(EXIT_FAILURE);
+    pthread_mutex_lock(&mutex);
+    while((pump->state & BIT(1)) != 0) pthread_cond_wait(&is_level, &mutex);
+    get_temp(pump, F);
+    pthread_mutex_unlock(&mutex);
+    return (void *)pump;
 }
 
 int main(void)
 {
     Pump *pump = NULL;
     Temp temp = F;
+    pthread_t device;
+    pthread_t user;
+    pthread_t report;
     init(&pump, temp);
     printf("Pump allocated at address %p with temp %.2u\n", pump, pump->temp);
+    // Spawn the workers 
+    pthread_create(&device, NULL, fill_water, (void *)pump);
+    pthread_create(&user, NULL, get_water, (void *)pump);
+    pthread_create(&report, NULL, show_report, (void *)pump);
+    // join the threads 
+    pthread_join(device, (void **)pump);
+    pthread_join(user, (void **)pump);
+    pthread_join(report, (void **)pump);
+
+
+
     free(pump);
     return 0;
 }
