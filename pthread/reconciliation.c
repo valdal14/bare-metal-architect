946cb6a4d3bc2f52179bc9ef125a06f155f061b5
Valerio DAlessio <u105550@canda.com>
2026-06-29 11:40:05 +0200

    feat(thread) POSIX Thread Example - Add PipelineData struct

--- pthread/reconciliation.c
+++ pthread/reconciliation.c
@@ -5,6 +5,27 @@
 #include <pthread.h>
 #include <unistd.h>
 
+typedef struct 
+{
+    /**
+     * 8 bytes
+     * 7 byte
+     * 1 byte
+     * -------
+     * 16 bytes + 0 paddings = 16 bytes 
+     */
+    struct PipelineData *next;
+    char cc_code[7];
+    /**
+     * bit 0 source
+     * bit 2 destination
+     * bit 4 processed
+     */
+    uint8_t metadata;
+
+} PipelineData;
+
+
 int main(void)
 {
     return 0;
