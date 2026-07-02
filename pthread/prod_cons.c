e8bfee07b857dc0f4aef32ddabb2554159931846
Valerio DAlessio <u105550@canda.com>
2026-07-02 06:33:09 +0200

    feat(thread) POSIX Thread Producer and Consumer - Add add_view method

--- pthread/prod_cons.c
+++ pthread/prod_cons.c
@@ -8,6 +8,9 @@
 
 #define VIEWS_COUNT 4
 
+// Concurrency Primitives
+pthread_mutex_t views_lock = PTHREAD_MUTEX_INITIALIZER;
+
 /* File-scoped static array. Kept private to this file. */
 static const char *const DB_VIEWS[VIEWS_COUNT] = {
     "users_view",
@@ -21,7 +24,6 @@ typedef struct Data
     // 17 + 7 paddings = 24 bytes 
     struct Data *next;
     char *view;
-    uint8_t index;
 } Data;
 
 typedef struct List
@@ -70,10 +72,9 @@ uint8_t hash_function(const char *key) {
  * @param const char key pointer 
  * @return bool
  */
-bool verify_view_by(const char *key)
+bool verify_view(const char *key)
 {
     uint8_t view_idx = hash_function(key);
-    printf("idx = %d\n", view_idx);
     const char *view = DB_VIEWS[view_idx];
    
     if(strcmp(key, view) == 0)
@@ -96,7 +97,7 @@ void init_producer(Producer **producer)
     
     if(p == NULL) 
     {
-        fprintf(stderr, "Could not allocated space for the Producer\n");
+        fprintf(stderr, "Could not allocate space for the Producer\n");
         exit(EXIT_FAILURE);
     }
 
@@ -104,8 +105,24 @@ void init_producer(Producer **producer)
 
     if(p->list == NULL)
     {
-        fprintf(stderr, "Could not allocated space for the Producer's List\n");
+        fprintf(stderr, "Could not allocate space for the Producer's List\n");
         exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < VIEWS_COUNT; i++)
+    {
+        List *new_list = (List *)malloc(sizeof(List));
+        new_list->head = NULL;
+        new_list->tail = NULL;
+
+        if(new_list == NULL)
+        {
+            fprintf(stderr, "Could not allocate space for the List\n");
+            exit(EXIT_FAILURE);
+        }
+
+        p->list[i] = new_list;
+
     }
 
     p->capacity = VIEWS_COUNT;
@@ -114,12 +131,73 @@ void init_producer(Producer **producer)
     *producer = p;
 }
 
+/**
+ * @brief The purpose of this method is to add a new view to the 
+ * List of views that needs to be processed. Uses a callback to 
+ * evaluate whether the given view's name is in the list of the 
+ * supported views. 
+ *
+ * @param Producer producer pointer
+ * @param const char view_name pointer. The view's name 
+ * @param bool(*verify)(const char *view_name). The verify callback
+ * @return void
+ */
+void add_view(Producer *producer, const char *view_name, bool(*verify)(const char *view_name))
+{
+    uint8_t is_valid = verify(view_name);
+    
+    if(is_valid == false)
+    {
+        fprintf(stderr, "ERROR: %s is not a valid view\n", view_name);
+        fprintf(stderr, "%s will not be processed\n", view_name);
+        return;
+    }
+    
+    // allocate space for the data 
+    Data *data = (Data *)malloc(sizeof(Data));
+    
+    if(data == NULL)
+    {
+        fprintf(stderr, "Could not allocate space for the view %s\n", view_name);
+        fprintf(stderr, "%s will not be processed\n", view_name);
+        return;
+    }
+
+    // copy the verified view_name
+    size_t view_length = (strlen(view_name) + 1);
+    data->view = (char *)malloc(sizeof(char) * view_length);
+    strncpy(data->view, view_name, view_length);
+    data->view[view_length] = '\0';
+    
+    data->next = NULL;
+    uint8_t list_idx = hash_function(data->view);
+
+    if(producer->list[list_idx]->head == NULL)
+    {
+        producer->list[list_idx]->head = data;
+        producer->list[list_idx]->tail = data;
+    }
+    else
+    {
+        List *list = producer->list[list_idx];
+        list->tail->next = data;
+        list->tail = data;
+    }
+}
+
 int main(void)
 {
     Producer *producer = NULL;
     init_producer(&producer);
     printf("Producer allocated at address %p\n", producer);
-    bool is_valid = verify_view_by("logs_view");
-    is_valid == true ? printf("found it\n") : printf("Not found\n");
+    add_view(producer, "logs_view", verify_view); 
+    add_view(producer, "inventory_view", verify_view); 
+    add_view(producer, "orders_view", verify_view); 
+    add_view(producer, "users_view", verify_view); 
+    
+    printf("%s\n", producer->list[0]->head->view);
+    printf("%s\n", producer->list[0]->tail->view);
+
+    
     return 0;
 }
