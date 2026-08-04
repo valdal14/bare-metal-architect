f4a5c4eaa3eabecdf86c35cc58ead84cbbb63720
Valerio DAlessio <u105550@canda.com>
2026-08-04 08:10:19 +0200

    feat(system) C Language Recap - Add the add method and realloc callback

--- recap/custom_arena.c
+++ recap/custom_arena.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 
 typedef struct
 {
     uint8_t *buffer;
     size_t capacity;
     size_t offset;
+    size_t size;
 
 } Arena;
 
@@ -18,7 +20,7 @@ typedef struct
  */
 void alloc(Arena **arena, size_t capacity)
 {
-    Arena *new_arena = (Arena *)calloc(1, sizeof(Arena *));
+    Arena *new_arena = (Arena *)calloc(1, sizeof(Arena));
     
     if(new_arena == NULL)
     {
@@ -36,14 +38,59 @@ void alloc(Arena **arena, size_t capacity)
 
     new_arena->capacity = capacity;
     new_arena->offset = 0;
-
+    new_arena->size = 0;
     *arena = new_arena;
+}
+
+/**
+ * @brief Attempts to realloc the Arena's buffer 
+ * @param Arena arena pointer
+ * @return void
+ */
+void buffer_realloc(Arena *arena)
+{
+    void *op = realloc(arena->buffer, (arena->capacity * 2));
+    
+    if(op == NULL)
+    {
+        fprintf(stderr, "Buffer Reallocation Failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * @brief Add a new values to the Arena's Buffer
+ * @param Arena arena pointer
+ * @param uint8_t val
+ * @param void(*on_full)(Arena *arena) realloc callback
+ * @return void
+ */
+void add(Arena *arena, uint8_t val, void(*on_full)(Arena *arena))
+{
+    if((arena->size + 1) > arena->capacity)
+    {
+        printf("Arena Buffer Full:\n");
+        printf("Reallocating Space for the Buffer. Please Wait...\n");
+        sleep(1);
+        on_full(arena);
+        printf("Reallocation Completed Successfully");
+    }
+
+    arena->buffer[arena->offset] = val;
+    // the size of this is determined by the size of the type stored 
+    // inside the Arena's buffer 
+    size_t type_size = sizeof(uint8_t);
+    arena->offset += 1;
+    arena->size += type_size;
 }
 
 int main(void)
 {
     Arena *arena = NULL;
-    alloc(&arena, 254);
+    alloc(&arena, 3);
     printf("Arena allocated at address %p\n", arena);
+    printf("Arena Capacity = %lu\n", arena->capacity);
+    printf("Arena Size = %lu\n", arena->size);
+    add(arena, 14, buffer_realloc);
     return 0;
 }
